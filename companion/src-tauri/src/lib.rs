a8230b1eb8566a88bd8f694f151e34dc7d118bb7
asthrun <62132851+asthrun@users.noreply.github.com>
2026-09-02 20:55:52 +0200

    Add tray-only Companion lifecycle and website launcher

--- companion/src-tauri/src/lib.rs
+++ companion/src-tauri/src/lib.rs
@@ -1,10 +1,16 @@
+
+use tauri::{
+    menu::{Menu, MenuItem},
+    tray::TrayIconBuilder,
+    Manager, WindowEvent,
+};
+use tauri_plugin_opener::OpenerExt;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
-use tauri::Manager;
-
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -16,6 +22,70 @@ pub fn run() {
             }
         }))
         .plugin(tauri_plugin_opener::init())
+        .setup(|app| {
+            if let Some(window) = app.get_webview_window("main") {
+                let window_for_close = window.clone();
+
+                window.on_window_event(move |event| {
+                    if let WindowEvent::CloseRequested { api, .. } = event {
+                        api.prevent_close();
+                        let _ = window_for_close.hide();
+                    }
+                });
+            }
+
+            let open_item = MenuItem::with_id(
+                app,
+                "open",
+                "Open TFDSpeedrun",
+                true,
+                None::<&str>,
+            )?;
+
+            let hotkeys_item = MenuItem::with_id(
+                app,
+                "hotkeys",
+                "Hotkeys: Disabled",
+                false,
+                None::<&str>,
+            )?;
+
+            let close_item = MenuItem::with_id(
+                app,
+                "close",
+                "Close Companion",
+                true,
+                None::<&str>,
+            )?;
+
+            let menu = Menu::with_items(
+                app,
+                &[&open_item, &hotkeys_item, &close_item],
+            )?;
+
+            TrayIconBuilder::new()
+            .icon(app.default_window_icon().unwrap().clone())
+            .tooltip("TFDSpeedrun Companion")
+            .menu(&menu)
+            .show_menu_on_left_click(false)
+            .on_menu_event(|app, event| {
+                match event.id.as_ref() {
+                    "open" => {
+                        let _ = app.opener().open_url(
+                            "https://tfdspeedrun.vercel.app",
+                            None::<&str>,
+                        );
+                    }
+                    "close" => {
+                        app.exit(0);
+                    }
+                    _ => {}
+                }
+            })
+            .build(app)?;
+
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
